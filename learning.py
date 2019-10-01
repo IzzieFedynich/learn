84e27b6ea43226c374364cb7b0bad89427b63080
IzzieFedynich <54276173+IzzieFedynich@users.noreply.github.com>
2019-10-01 08:32:14 -0500

    Update learning.py

--- learning.py
+++ learning.py
@@ -1,4 +1,22 @@
-print("hello world")
-print("Howdy, y'all")
+# this is an octothorpe -- it makes a comment (line ignored by the computer)
+# print("hello world!")
+# testing out print
+print("hello there!")
+print("Howdy, y'all!")
 print("I like typing this.")
 print("This is fun.")
+
+# math skills demo
+
+# counting the chickens
+print("I will not count my chickens", 2)
+# adding hens
+print("Hens: ", 25+30/6)
+# adding roosters
+print("Roosters: ", 100 - 25 * 3 % 4)
+# counting the eggs
+print("Now I will count the eggs", 7)
+# adding the eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# math problem
+print(3 + 2 < 5 - 7)
